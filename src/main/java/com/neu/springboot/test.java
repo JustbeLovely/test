ed98328e5228570d924f1ab1fe9ce253278c4c4c
JustbeLovely <15032336139@163.com>
2018-11-20 14:54:39 +0800

    helloController

--- src/main/java/com/neu/springboot/test.java
+++ src/main/java/com/neu/springboot/test.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class test {
 
-
-
-
     @RequestMapping("/test")
     public String  test(){
         return "测试11";
